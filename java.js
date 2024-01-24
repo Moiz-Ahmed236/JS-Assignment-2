f3d93cd71211033c59eba60d8720caa332fc7127
Moiz-Ahmed236 <155655860+Moiz-Ahmed236@users.noreply.github.com>
2024-01-24 10:06:13 -0800

    Java Code

---
+++ java.js
@@ -0,0 +1,11 @@
+// Area of Rectangle:
+let length=15;
+let width=10;
+
+let area=(length*width);
+console.log(area);
+
+// Ask User Name and Alert Message:
+let userName=prompt("Your Name")
+alert("I'm Moiz Ahmed; Hope you all are having a great Day! I am Working on a Assignment that connects to my website")
+console.log(userName);
